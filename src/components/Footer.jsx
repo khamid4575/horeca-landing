6b5a83b8d2ded2354572cb79f7973221e8e651be
Abdurahmon <ibrohimovabdurahmon68@gmail.com>
2024-07-01 10:57:30 +0500

    finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
   const { t } = useTranslation("global");
   return (
     <footer className="bg-black text-white pt-8 pb-4">
-      <div className="flex 2xl:w-[1700px] lg:w-[1200px] mx-auto ">
-        <div className=" w-2/5">
+      <div className="flex 2xl:w-[1700px] lg:w-[1200px] lg:flex-row flex-col mx-auto lg:text-start text-center">
+        <div className="lg:w-2/5">
           <h1 className="text-4xl font-bold">+998 (71) 888-88-88</h1>
-          <div className="my-5 flex justify-center items-center w-[62%]">
+          <div className="my-5 flex justify-center items-center lg:w-[62%]">
             <a href="#">
               <i class="bx bxl-linkedin text-2xl"></i>
             </a>
@@ -25,8 +25,8 @@ const Footer = () => {
           </p>
         </div>
       </div>
-      <div className="flex 2xl:w-[1700px] lg:w-[1200px] mx-auto  justify-start items-start">
-        <p className="w-2/5">
+      <div className="flex lg:mt-0 mt-5 lg:flex-row flex-col 2xl:w-[1700px] lg:w-[1200px] mx-auto lg:text-start text-center">
+        <p className="lg:w-2/5">
           © Supply Partners 2024. {t("footer.allRightsReserved")}
         </p>
         <p className="">{t("footer.personalDataPolicy")}</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [isPastFirstSection, setIsPastFirstSection] = useState(false);
   const [isLanguageDropdownOpen, setIsLanguageDropdownOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const languageDropdownRef = useRef(null);
 
   const links = [
@@ -59,22 +60,30 @@ const Navbar = () => {
     setIsLanguageDropdownOpen(false);
   };
 
+  const handleSidebarToggle = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleSidebarClose = () => {
+    setIsSidebarOpen(false);
+  };
+
   const LanguageSelector = () => (
     <div className="relative" ref={languageDropdownRef}>
       <button
         onClick={toggleLanguageDropdown}
-        className="mr-5 flex items-center text-white px-4 rounded"
+        className="flex items-center text-white rounded"
       >
-        {i18n.language === "ru" && <Flag country="RU" className="mr-2 w-10" />}
-        {i18n.language === "uz" && <Flag country="UZ" className="mr-2 w-10" />}
+        {i18n.language === "ru" && <Flag country="RU" className="w-10" />}
+        {i18n.language === "uz" && <Flag country="UZ" className="w-10" />}
       </button>
       {isLanguageDropdownOpen && (
-        <div className="absolute right-2 2xl:mt-8 z-50 mt-3 w-28 bg-white rounded shadow-lg">
+        <div className="absolute lg:right-0 2xl:mt-5 z-50 mt-4 w-28 lg:bg-white rounded lg:shadow-lg">
           {languages.map((language) => (
             <li
               key={language.code}
               onClick={() => handleLanguageChange(language.code)}
-              className="flex font-medium items-center px-3 py-2 cursor-pointer text-gray-500 hover:bg-gray-200"
+              className="flex font-medium items-center px-3 py-2 cursor-pointer rounded text-white lg:text-gray-500 hover:bg-gray-200 hover:text-black"
             >
               <Flag country={language.country} className="mr-2" />
               {language.text}
@@ -86,59 +95,95 @@ const Navbar = () => {
   );
 
   return (
-    <nav
-      className={`py-5 z-50 ${
-        isScrolled ? "bg-gray-50 sticky top-0" : "bg-transparent"
-      }`}
-    >
-      <div className="flex justify-between items-center 2xl:w-[1700px] lg:w-[1200px] w-full mx-auto lg:px-0 px-5">
-        <h1
-          className={`uppercase text-2xl font-extrabold  ${
-            !isScrolled && "text-white"
-          }`}
-        >
-          <a href="#" className="z-50">
-            Supply partners
-          </a>
-        </h1>
-        <div className="flex">
-          <div
-            className={`hidden z-50 lg:flex gap-8 ${
-              isScrolled ? "mr-0" : "mr-20"
+    <>
+      <nav
+        className={`py-5 z-50 ${
+          isScrolled ? "bg-gray-50 sticky top-0" : "bg-transparent"
+        }`}
+      >
+        <div className="flex justify-between items-center 2xl:w-[1700px] lg:w-[1200px] w-full mx-auto lg:px-0 px-5">
+          <h1
+            className={`uppercase text-2xl font-extrabold  ${
+              !isScrolled && "text-white"
             }`}
           >
-            {links.map((link, index) => (
+            <a href="#" className="z-50">
+              Supply partners
+            </a>
+          </h1>
+          <div className="flex">
+            <div
+              className={`hidden z-50 lg:flex gap-8 ${
+                isScrolled ? "mr-0" : "mr-20"
+              }`}
+            >
+              {links.map((link, index) => (
+                <a
+                  key={index}
+                  className={`text-xl font-medium ${
+                    !isScrolled && "text-white"
+                  }`}
+                  href={`#${link.path}`}
+                >
+                  {link.name}
+                </a>
+              ))}
+            </div>
+            {!isPastFirstSection && (
+              <div className={`hidden z-50 lg:flex ${isScrolled && "hidden"}`}>
+                <LanguageSelector />
+              </div>
+            )}
+            <div className="lg:hidden flex items-center justify-end z-30">
+              <Button
+                variant={`${isScrolled ? "scroll" : "phone"}`}
+                onClick={handleSidebarToggle}
+                aria-expanded={isSidebarOpen}
+                aria-controls="mobile-menu"
+              >
+                <i className="bx bx-menu text-3xl w-full"></i>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 right-0 h-full w-64 bg-black text-white shadow-lg transform ${
+          isSidebarOpen ? "translate-x-0" : "translate-x-full"
+        } transition-transform duration-300 ease-in-out z-50`}
+      >
+        <div className="flex justify-between items-center p-5">
+          {/* <LanguageSelector /> */}
+          {isSidebarOpen && <LanguageSelector />}
+          <button onClick={handleSidebarClose} className="text-3xl">
+            &times;
+          </button>
+        </div>
+        <ul className="flex flex-col p-5 mt-10 list-none">
+          {links.map((link, index) => (
+            <li key={index} className="py-2">
               <a
-                key={index}
-                className={`text-xl font-medium ${!isScrolled && "text-white"}`}
                 href={`#${link.path}`}
+                className="text-lg"
+                onClick={handleSidebarClose}
               >
                 {link.name}
               </a>
-            ))}
-          </div>
-          {!isPastFirstSection && (
-            <div
-              className={`hidden z-50 lg:flex text-white ${
-                isScrolled && "hidden"
-              }`}
-            >
-              <LanguageSelector />
-            </div>
-          )}
-          <div className="lg:hidden flex items-center justify-end z-50">
-            <Button
-              variant={`${isScrolled ? "scroll" : "phone"}`}
-              // onClick={toggleDropdown}
-              // aria-expanded={isDropdownOpen}
-              aria-controls="mobile-menu"
-            >
-              <i className="bx bx-menu text-3xl w-full"></i>
-            </Button>
-          </div>
-        </div>
+            </li>
+          ))}
+        </ul>
       </div>
-    </nav>
+
+      {/* Overlay */}
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black opacity-50 z-40"
+          onClick={handleSidebarClose}
+        ></div>
+      )}
+    </>
   );
 };
 
